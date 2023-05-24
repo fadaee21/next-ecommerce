97e012b0b5e88696677f43388d8929c495d5cbfb
ARASH FAD <fadarash@gmail.com>
2023-05-24 15:08:11 +0330

    A

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import "../styles/globals.css";
+import "bootstrap/dist/css/bootstrap.rtl.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap.bundle.js" as any);
+  }, []);
+
+  return <Component {...pageProps} />;
 }
